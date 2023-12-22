b36c352453aaf488fe2fb477d0128de82ec50958
unknown <ratiry@yandex.ru>
2023-12-22 13:15:35 +0300

    before changing edition of popyp

--- src/Pages/Collections/AddNewCollection/AddNewCollection.jsx
+++ src/Pages/Collections/AddNewCollection/AddNewCollection.jsx
@@ -2,7 +2,7 @@ import classes from "./AddnewCollection.module.scss";
 let AddNewCollection=(props)=>{
   return(
     <div onClick={props.onClick} className={classes.AddNewCollection} >
-      +
+      <span>+</span>
     </div>
   )
 }
