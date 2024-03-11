ab9a02a2fc57e02008647916c1b9484de731ac9a
unknown <ratiry@yandex.ru>
2024-03-11 12:22:27 +0300

    merge

--- src/common/Popup/Popup.jsx
+++ src/common/Popup/Popup.jsx
@@ -2,7 +2,7 @@ import classes from "./Popup.module.scss";
 let Popup = (props) => {
   return (
     <>
-      <div className={classes.popup}>{props.children}</div>
+      <div className={classes.popup}>{props.children}</div> 
     </>
   );
 };
